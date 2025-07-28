68c7f263afa2f40ea6010233771f6a1e978594f6
yuiyamu <yuiyakuki@gmail.com>
2025-07-28 11:33:30 -0500

    changed exports for esm

--- main.ts
+++ main.ts
@@ -10,15 +10,15 @@ const difficultyColourSpectrum = scaleLinear<string>()
   .range(['#4290FB', '#4FC0FF', '#4FFFD5', '#7CFF4F', '#F6F05C', '#FF8068', '#FF4E6F', '#C645B8', '#6563DE', '#18158E', '#000000'])
   .interpolate(interpolateRgb.gamma(2.2));
 
-exports.colourize = function (difficulty: number) {
+export function colourize(difficulty: number) {
   return difficultyColourSpectrum(difficulty);
 }
 
-exports.colourizeHex = function (difficulty: number) {
+export function colourizeHex(difficulty: number) {
   return color(difficultyColourSpectrum(difficulty))!.formatHex(); //should always get something because of clamp(true), so it's fine to supress this
 }
 
-exports.colourizeRawRgb = function (difficulty: number) {
+export function colourizeRawRgb(difficulty: number) {
   //the other exports are rather self explanitory, but this should return an array of numbers in case it's more beneficial to work with that~
   let colour = difficultyColourSpectrum(difficulty); //because d3 colours return back as rgb(RRR, GGG, BBB), we need to remove the extra fluff ^-^
   let colourArray: number[] = [];
